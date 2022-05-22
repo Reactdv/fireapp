37a7884ed8740b475a31bbcd25445c6bb4b0487d
Reactdv <104239643+Reactdv@users.noreply.github.com>
2022-05-23 04:04:37 +0800

    Add files via upload

---
+++ Components/colors.js
@@ -0,0 +1,16 @@
+
+ const colors ={
+  
+  background:"#00ccff",
+  primary:"#40ff00",
+  secondary:"#0800ff",
+  tertiary:"#fff900",
+  subTitle:"#cfd4d5cc",
+  purple:"#ba40f5",
+  pink:"#ff00b7",
+  red:"#ff0043",
+  blueGreen:"#00ffe9",
+  seaGreen:"#2E8B57",
+  
+}
+export default colors;
